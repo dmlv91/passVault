ab354f4a8ae480e77b545a5dd1b62bbb2abfec7c
DAVIS MEDINS <davsm@mikrotik.com>
2022-12-02 12:37:18 +0200

    update passGenerator functions

--- passGenerator.py
+++ passGenerator.py
@@ -13,16 +13,15 @@ password = ""
 def generator() :
     
     print("Pleace choose your password attributes:")
-    passLength = int(input("Password length: \n"))
-
-    if passLength == 0:
+    passLength = input("Password length: \n").strip()
+    if int(passLength) <= 0:
         print("Invalid input")
         generator()
     else:
         print(f'Password length set to {passLength}')
 
     parameters = []
-    parameters = input("Select password features, separate with comma: \n \n" 
+    parameters = input("Select password features, separate with space: \n \n" 
         + "Upper case letters    1 \n"
         + "Numbers               2 \n"
         + "Special characters    3 \n"
@@ -30,14 +29,18 @@ def generator() :
         + "None                  0 \n"
         + "Exit program          99 \n")
 
-    if int(parameters) !=99 and int(parameters) >4:
-        print("Invalid parameters selected!!!")
-        generator()
-    elif int(parameters) != 99:
-        print(f'Your choice = {parameters}')
-
+    parameters = parameters.split(' ')
+    parameters = list(map(int, parameters))
+    for attribute in parameters:
+        if attribute < 0:
+            print("Invalid input")
+            generator()
+        if attribute !=99 and attribute >4 and attribute <0:
+            print("Invalid parameters selected!!!")
+            generator()
+        elif attribute != 99:
+            print(f'Your choice = {attribute}')
     
-    parameters = parameters.split(',')
     if len(parameters) == 1:
         for attribute in parameters:
             if int(attribute) == 0:
@@ -52,9 +55,19 @@ def generator() :
                 password = ''.join(secrets.choice(numbers + lettersBoth + specialChars) for i in range(passLength))
             elif int(attribute) == 99:
                 sys.exit()
-    elif len(parameters) > 1:
+    else:
+        if 1 not in parameters and 2 not in parameters and 3 not in parameters:
+            print("Out of bounds")
+            generator()
+        attributes = []
         for attribute in parameters:
-            print("more than one choice")
+            attributes.append(attribute)
+        if 1 in attributes and 2 in attributes:
+            password = ''.join(secrets.choice(numbers + lettersBoth) for i in range(passLength))
+        elif 1 in attributes and 3 in attributes:
+            password = ''.join(secrets.choice(lettersBoth + specialChars) for i in range(passLength))
+        elif 2 in attributes and 3 in attributes:
+            password = ''.join(secrets.choice(lettersLow + numbers + specialChars) for i in range(passLength))
     if password != "":
         print(f'Your password is {password}')
 
