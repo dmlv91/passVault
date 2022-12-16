be2d3671e1a9628e0c9f85807f202ee742bb46c7
DAVIS MEDINS <davsm@mikrotik.com>
2022-12-16 18:59:52 +0200

    frontend structure

---
+++ web/models.py
@@ -0,0 +1,18 @@
+from . import db
+from flask_login import UserMixin
+from sqlalchemy.sql import func
+
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(50), unique=True)
+    firstName = db.Column(db.String(30))
+    lastName = db.Column(db.String(50))
+    password = db.Column(db.String(30))
+    passwords = db.relationship('Vault')
+
+class Vault(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    userID = db.Column(db.Integer, db.ForeignKey('user.id'))
+    service = db.Column(db.String(50))
+    passw = db.Column(db.String(50))
+    date = db.Column(db.DateTime(timezone=True), default=func.now())
--- web/views.py
+++ web/views.py
@@ -1,7 +1,22 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, flash
+from flask_login import login_required, current_user
+from .models import Vault
+from . import db
 
 views = Blueprint('views', __name__)
 
-@views.route('/')
+@views.route('/', methods=['GET','POST'])
+@login_required
 def home():
-    return render_template("home.html")
+    if request.method == 'POST':
+        passEntry = request.form.get('Vault')
+
+        if len(passEntry) < 1:
+            flash('error', category='error')
+        else:
+            newEntry = Vault(passw=passEntry, user_id=current_user.id)
+            db.session.add(newEntry)
+            db.session.commit()
+            flash('Success', category='success')
+
+    return render_template("home.html", user=current_user)
